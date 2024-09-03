64fce18a85aef54c837f7b4814f991c9678e7bc6
KIMBUMSU123 <88920973+KIMBUMSU123@users.noreply.github.com>
2024-09-03 16:55:18 +0900

    최준하 OrdersController

--- main/java/com/mycom/myapp/controller/OrdersController.java
+++ main/java/com/mycom/myapp/controller/OrdersController.java
@@ -1,47 +1,30 @@
 package com.mycom.myapp.controller;
-
-import java.util.List;
-
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
+import org.springframework.web.bind.annotation.*;
 import com.mycom.myapp.dto.OrdersCreateDto;
 import com.mycom.myapp.dto.OrdersDetailResponseDto;
 import com.mycom.myapp.dto.OrdersResponseDto;
 import com.mycom.myapp.dto.OrdersUserProductDto;
 import com.mycom.myapp.service.OrdersService;
-
 import lombok.RequiredArgsConstructor;
-
-@Controller
-@ResponseBody
-@RequestMapping("/orders")
+import java.util.List;
+@RestController
+@RequestMapping("/api")
 @RequiredArgsConstructor
 public class OrdersController {
-
-	private final OrdersService ordersService;
-	
-	@PostMapping
-    public ResponseEntity<OrdersResponseDto> createOrder(@RequestBody OrdersCreateDto ordersCreateDto) {
-        OrdersResponseDto createdOrder = ordersService.createOrder(ordersCreateDto);
-        return ResponseEntity.ok(createdOrder);
+    private final OrdersService ordersService;
+    
+    @PostMapping("/orders")
+    public OrdersResponseDto createOrder(@RequestBody OrdersCreateDto ordersCreateDto) {
+        return ordersService.createOrder(ordersCreateDto);
     }
-	
-	@GetMapping("/listordersuserproduct")
-	public List<OrdersUserProductDto> listOrdersUserProduct() {
-		List<OrdersUserProductDto> ordersUserProductDtoList = ordersService.listOrdersUserProduct();
-		return ordersUserProductDtoList;
-	}
-	
-    @GetMapping("/detail/{orderId}")
-    public ResponseEntity<OrdersDetailResponseDto> getOrderDetail(@PathVariable("orderId") int orderId) {
-        OrdersDetailResponseDto orderDetail = ordersService.getOrderDetail(orderId);
-        return ResponseEntity.ok(orderDetail);
+    
+    @GetMapping("/orders/listordersuserproduct")
+    public List<OrdersUserProductDto> listOrdersUserProduct() {
+        return ordersService.listOrdersUserProduct();
+    }
+    
+    @GetMapping("/orders/detail/{orderId}")
+    public OrdersDetailResponseDto getOrderDetail(@PathVariable("orderId") int orderId) {
+        return ordersService.getOrderDetail(orderId);
     }
 }
